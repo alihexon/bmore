67cdeb2862be6d0742fe0dc0cecafda4d862f720
hexoN <aliakbaripour00@gmail.com>
2024-03-13 10:01:37 +0330

    style(hamburger-animation): Added an animation for opening and closing the hamburger menu

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const closeButton = document.querySelector('.close-btn');
 
 function openPopup() {
   hamburgerPopup.classList.add('is-visible');
+  hamburgerPopup.classList.remove('is-hidden-animation');
   hamburgerPopupOverlay.classList.add('is-visible');
 }
 
 function closePopup() {
+  hamburgerPopup.classList.add('is-hidden-animation');
   hamburgerPopup.classList.remove('is-visible');
   hamburgerPopupOverlay.classList.remove('is-visible');
 }
