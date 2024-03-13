9bf9229e283e91caa2986971e31a9ff7e8fd4ce5
hexoN <aliakbaripour00@gmail.com>
2024-03-13 11:34:58 +0330

    style(main-content-why-us): Added why us script and html

--- script.js
+++ script.js
@@ -17,3 +17,43 @@ function closePopup() {
 
 hamburgerElement.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
+
+
+
+// why-us scripts
+
+const options = document.querySelectorAll('#main-header ul li');
+const displayText = document.querySelector('.js-display');
+
+showOption('staff');
+
+function showOption(selectedOption) {
+  option = document.querySelector(`.${selectedOption}`);
+
+  turnOffSelectedOption();
+  option.classList.toggle('js-chosen');
+
+  displayParagraph(selectedOption);
+}
+
+function turnOffSelectedOption() {
+  const selectedOption = document.querySelector('.js-chosen')
+
+  if (selectedOption) {
+    selectedOption.classList.remove('js-chosen')
+  }
+}
+
+function displayParagraph(context) {
+  if (context === 'staff') {
+    displayText.innerHTML = "Our dedicated team is always on hand to ensure a smooth and enjoyable experience for all players on Baltimore RP."
+  } else if (context === 'unique') {
+    displayText.innerHTML = "Baltimore RP offers a one-of-a-kind experience you won't find anywhere else."
+  } else if (context === 'community') {
+    displayText.innerHTML = "We foster a welcoming and respectful environment where fair play is paramount."
+  } else if (context === 'stability') {
+    displayText.innerHTML = "Baltimore RP is built on a strong foundation, ensuring a smooth and lag-free gameplay experience."
+  } else if (context === 'fair-play') {
+    displayText.innerHTML = "We take a zero-tolerance stance on cheating and griefing, guaranteeing a level playing field for everyone."
+  } 
+}
